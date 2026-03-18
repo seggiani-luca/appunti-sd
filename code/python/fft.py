f3befb3a6c4407826e8452c45e204742fb78cda9
Luca Seggiani <seggianiluca2004@gmail.com>
2026-03-19 00:07:32 +0100

    Lezioni ultime riviste

--- code/python/fft.py
+++ code/python/fft.py
@@ -3,12 +3,21 @@ import cmath
 def hann(x):
     ln = len(x)
     def weight(n):
-        return 0.5 - 0.5 * math.cos(2 * math.pi * n / (ln - 1))
+        return 0.5 - 0.5 * cmath.cos(2 * cmath.pi * n / (ln - 1))
 
     return [y * weight(n) for n, y in enumerate(x)]
 
 def db(x):
-    return [20 * math.log10(abs(y) + 1e-12) for y in x]
+    return [20 * cmath.log10(abs(y) + 1e-12) for y in x]
+
+def unwrap(x):
+    def unwrap_single(c):
+        while c > 3.1:
+            c -= 2 * cmath.pi
+        while c < -3.1:
+            c += 2 * cmath.pi
+        return c
+    return [unwrap_single(c) for c in x]
 
 def fft(x):
     ln = len(x)
@@ -25,7 +34,7 @@ def fft(x):
         # merge even/odd FFTS in result
         res = [None] * ln 
         for k in range(0, hln):
-            twid = cmath.exp(-2j * math.pi * k / ln)
+            twid = cmath.exp(-2j * cmath.pi * k / ln)
             res[k]       = even[k] + twid * odd[k] 
             res[k + hln] = even[k] - twid * odd[k] 
         return res
--- code/python/fft_fun.py
+++ code/python/fft_fun.py
@@ -1,34 +1,47 @@
 import matplotlib.pyplot as plt
 import fft
-import math
+import cmath
 
-# define function to run FFT on
-def func(x):
-    if x < 0 or x > 1:
-        return 0
-    return 1
+# define function to run fft on
+T = 1
+def step(t):
+    return 1 if t > 0 else 0
+def func(t):
+    return cmath.exp(-t / T) * step(t)  
 
 # sample function
 dt = 1 / 10
 window = 2 ** 8
-signal = [func(x * dt) for x in range(window)]
-times = [n * dt for n in range(window)]
+offs = window // 2
+signal = [func((n - offs) * dt) for n in range(window)]
+times = [(n - offs) * dt for n in range(window)]
 
-# get FFT
-spec = fft.fft(signal)[:window // 2]
+# get fft
+spec = fft.fft(signal)
+spec = spec[window // 2:] + spec[:window // 2]
+
+spec = [c if i % 2 == 0 else -c for i, c in enumerate(spec)]
+
 spec_mag = [abs(c) for c in spec]
-freqs = [k / (window * dt) for k in range(window // 2)]
+spec_arg = [cmath.phase(c) for c in spec]
+# spec_arg = fft.unwrap(spec_arg)
+freqs = [(k - offs) / (window * dt) for k in range(window)]
 
 # plot time and frequency graphs
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8,6))
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8,6))
 
 ax1.plot(times, signal)
-ax1.set_xlabel("Time (s)")
-ax1.set_ylabel("Amplitude")
+ax1.set_xlabel("time (s)")
+ax1.set_ylabel("amplitude")
 
 ax2.plot(freqs, spec_mag)
-ax2.set_xlabel("Frequency (Hz)")
-ax2.set_ylabel("Magnitude")
+ax2.set_xlabel("frequency (hz)")
+ax2.set_ylabel("magnitude")
+
+# ax3.plot(freqs, spec_arg)
+ax3.plot(freqs, spec_arg)
+ax3.set_xlabel("frequency (hz)")
+ax3.set_ylabel("phase")
 
 plt.tight_layout()
 plt.show()
